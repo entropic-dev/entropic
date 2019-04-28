3cfa9a9bd0d4fd9309a2f6b538e6fc3e9c070ce6
Chris Dickinson <christopher.s.dickinson@gmail.com>
2019-04-27 19:38:46 -0700

    fix: it sure helps to include the file you just wrote

--- src/registry/handlers/index.js
+++ src/registry/handlers/index.js
@@ -16,6 +16,6 @@ async function handler(req) {
 
   // NB(chrisdickinson): this is just a sketch of grabbing a direct connection
   // to postgres. Usually we'll be dealing with the ORM.
-  const { rows } = await client.query(`select 'hello world'`)
-  return new Response(JSON.stringify(rows))
+  const { rows } = await client.query(`select 'hello world'`);
+  return new Response(JSON.stringify(rows));
 }
