71a90fa9c650a32bcf4cbaf1075cdaaf99b641f4
C J Silverio <ceejceej@gmail.com>
2019-04-28 10:16:42 -0700

    Restored the work I did in package.json (#9)
    * Restored the work I did in package.json.
    
    Specifically, nice linting targets and stuff.
    Restructured the runner so it can listen on env-provided ports.
    Added postgres to the circleci config.
    
    * Accidentally the env vars on the test run.

--- src/registry/index.js
+++ src/registry/index.js
@@ -1,9 +1,16 @@
+#!/usr/bin/env node
 'use strict';
 
 require('dotenv').config();
 
 const { makeRequestHandler } = require('./utils');
+const micro = require('micro');
+const logger = require('pino')();
 const middleware = require('./middleware');
 const handlers = require('./handlers');
 
-module.exports = makeRequestHandler(handlers, middleware);
+const handler = makeRequestHandler(handlers, middleware);
+const server = micro((req, res) => handler(req, res));
+
+server.listen(process.env.PORT);
+logger.info(`listening on port: ${process.env.PORT}`);
