4291c0f175e93eecd1db207d244e4bd8338b1e6a
C J Silverio <ceejceej@gmail.com>
2019-04-29 19:24:44 -0700

    Make pacote really cache, by giving it a directory.
    The directory is specified an env var. If the process can't create it,
    it just crashes. This is correct for the moment.

--- src/registry/lib/cache.js
+++ src/registry/lib/cache.js
@@ -4,7 +4,18 @@
 // The filesystem edition is a thin wrapper around pacote.
 // The S3 edition is a little more... complex. Also unimplemented.
 
+const fs = require('fs');
+const mkdirp = require('mkdirp');
 const pacote = require('pacote');
+const path = require('path');
+
+const CACHE_DIR = path.resolve(process.env.CACHE_DIR || '../../legacy-cache');
+const OPTS = {
+  cache: CACHE_DIR
+};
+if (!fs.existsSync(CACHE_DIR)) {
+  mkdirp(CACHE_DIR);
+}
 
 module.exports = {
   manifest,
@@ -13,13 +24,13 @@ module.exports = {
 };
 
 async function manifest(spec) {
-  return await pacote.manifest(spec);
+  return await pacote.manifest(spec, OPTS);
 }
 
 async function packument(spec) {
-  return await pacote.packument(spec);
+  return await pacote.packument(spec, OPTS);
 }
 
 async function tarball(spec) {
-  return pacote.tarball.stream(spec);
+  return pacote.tarball.stream(spec, OPTS);
 }
