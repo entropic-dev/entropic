2b027baf5ef1e9848cbcbb32a53eba6ff5da910c
C J Silverio <ceejceej@gmail.com>
2019-05-04 12:22:41 -0700

    The simplest possible whitelist. (#19)
    Plus, a whole lot of improvements on the read-through cache for
    VCpm packages. We now handle errors thrown by pacote, and make some
    kind of reasonable response. I'm not sure it's the response the VC
    cli wants, but I can deal with that later.
    
    The whitelist is a newline-delimited list of package names, pointed
    to by the env var WHITELIST. That's it.

--- src/registry/lib/cache.js
+++ src/registry/lib/cache.js
@@ -5,32 +5,58 @@
 // The S3 edition is a little more... complex. Also unimplemented.
 
 const fs = require('fs');
-const mkdirp = require('mkdirp');
 const pacote = require('pacote');
 const path = require('path');
+const logger = require('pino')();
 
 const CACHE_DIR = path.resolve(process.env.CACHE_DIR || '../../legacy-cache');
 const OPTS = {
   cache: CACHE_DIR
 };
-if (!fs.existsSync(CACHE_DIR)) {
-  mkdirp(CACHE_DIR);
-}
+let WHITELIST;
 
 module.exports = {
+  initialize,
+  allowed,
   manifest,
   packument,
   tarball
 };
 
+function initialize() {
+  // We do not catch exceptions here because we want to crash if we can't operate.
+  if (process.env.WHITELIST) {
+    logger.info(`reading whitelist from ${process.env.WHITELIST}`);
+    const data = fs
+      .readFileSync(process.env.WHITELIST)
+      .toString()
+      .trim();
+    if (data.length > 0) {
+      WHITELIST = new Set(data.split(/\r?\n/));
+    }
+    logger.info(`${WHITELIST ? WHITELIST.size : 0} packages in our whitelist`);
+  }
+}
+
+function allowed(spec) {
+  if (WHITELIST && !WHITELIST.has(spec)) {
+    return false;
+  }
+  return true;
+}
+
 async function manifest(spec) {
-  return await pacote.manifest(spec, OPTS);
+  return pacote.manifest(spec, OPTS);
 }
 
 async function packument(spec) {
-  return await pacote.packument(spec, OPTS);
+  return pacote.packument(spec, OPTS);
 }
 
 async function tarball(spec) {
+  // So! The stream version of the tarball fetch does not error if this version isn't
+  // found. So we check the manifest for this spec, which will throw an error if it's
+  // not a real version. This doesn't handle straight-up 404s for missing tars...
+  await pacote.manifest(spec, OPTS);
   return pacote.tarball.stream(spec, OPTS);
 }
--- src/registry/server.js
+++ src/registry/server.js
@@ -5,9 +5,12 @@ require('dotenv').config();
 
 const micro = require('micro');
 const logger = require('pino')();
-const middleware = require('./middleware');
+const cache = require('./lib/cache');
 const router = require('./handlers')();
+const middleware = require('./middleware');
 const { makeRequestHandler } = require('./lib/request-handler');
+
+cache.initialize();
 
 const handler = makeRequestHandler(router, middleware);
 const server = micro((req, res) => handler(req, res));
