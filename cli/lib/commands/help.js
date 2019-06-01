d3dbbb6795b3222130ffe31b08faedde3ab70176
Todd Kennedy <toddself@users.noreply.github.com>
2019-06-01 13:42:52 -0700

    Add basic help showing avilable commands (#100)
    * Add basic help showing avilable commands
    
    If you don't provide a command to ask for help on, it should show the
    list of commands that are available
    
    * Add rc file location
    
    * fix typo
    
    * Add support for loading command help from disk

--- cli/lib/commands/help.js
+++ cli/lib/commands/help.js
@@ -1,8 +1,44 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const readdirAsync = promisify(fs.readdir);
+
+const userHome = require('user-home');
+
 module.exports = help;
 
 async function help(opts) {
-  console.log('help has not been implemented yet. You could build it!');
-  console.log('comands: download, login, publish, whoami');
+  const command = opts.argv[0];
+  if (!command) {
+    await showBasicHelp();
+  } else {
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(path.join(__dirname, `help-${command}.txt`))
+        .on('error', async err => {
+          if (err.code === 'ENOENT') {
+            console.log(
+              `help has not been implemented yet for ${command}. You could build it!`
+            );
+            await showBasicHelp();
+            return resolve();
+          }
+          reject(err);
+        })
+        .on('end', () => resolve())
+        .pipe(process.stdout);
+    });
+  }
+}
+
+async function showBasicHelp() {
+  const commands = (await readdirAsync(__dirname))
+    .filter(cmd => cmd.endsWith('.js'))
+    .map(cmd => `\t${cmd.split('.')[0]}`)
+    .join('\n');
+  console.log('Usage: ds <command>');
+  console.log('\nAvailable commands:');
+  console.log(commands);
+  console.log(`\nThe configuration is located at ${userHome}/.entropicrc `);
 }
