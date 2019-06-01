f00ea4a6526682129e6448f7a3a66a433a3f8a90
Todd Kennedy <todd@selfassembled.org>
2019-06-01 13:38:54 -0700

    Add support for loading command help from disk

--- cli/lib/commands/help.js
+++ cli/lib/commands/help.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const { promisify } = require('util');
-const readdirAsync = promisify(require('fs').readdir);
+const readdirAsync = promisify(fs.readdir);
 
 const userHome = require('user-home');
 
@@ -10,15 +12,33 @@ module.exports = help;
 async function help(opts) {
   const command = opts.argv[0];
   if (!command) {
-    const commands = await readdirAsync(__dirname);
-    console.log('Usage: ds <command>');
-    console.log('\nAvailable commands:');
-    console.log(commands.map(cmd => `\t${cmd.split('.')[0]}`).join('\n'));
-    console.log(`\nThe configuration is located at ${userHome}/.entropicrc `);
+    await showBasicHelp();
   } else {
-    console.log(
-      `help has not been implemented yet for ${command}. You could build it!`
-    );
-    console.log('commands: download, login, publish, whoami');
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(path.join(__dirname, `help-${command}.txt`))
+        .on('error', async err => {
+          if (err.code === 'ENOENT') {
+            console.log(
+              `help has not been implemented yet for ${command}. You could build it!`
+            );
+            await showBasicHelp();
+            return resolve();
+          }
+          reject(err);
+        })
+        .on('end', () => resolve())
+        .pipe(process.stdout);
+    });
   }
+}
+
+async function showBasicHelp() {
+  const commands = (await readdirAsync(__dirname))
+    .filter(cmd => cmd.endsWith('.js'))
+    .map(cmd => `\t${cmd.split('.')[0]}`)
+    .join('\n');
+  console.log('Usage: ds <command>');
+  console.log('\nAvailable commands:');
+  console.log(commands);
+  console.log(`\nThe configuration is located at ${userHome}/.entropicrc `);
 }
