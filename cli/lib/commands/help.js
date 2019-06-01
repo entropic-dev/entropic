b45d2ce1c18b9f4252bd43b98a145ff1b2ff38ca
Todd Kennedy <todd@selfassembled.org>
2019-06-01 11:50:03 -0700

    Add rc file location

--- cli/lib/commands/help.js
+++ cli/lib/commands/help.js
@@ -3,6 +3,8 @@
 const { promisify } = require('util');
 const readdirAsync = promisify(require('fs').readdir);
 
+const userHome = require('user-home');
+
 module.exports = help;
 
 async function help(opts) {
@@ -12,6 +14,7 @@ async function help(opts) {
     console.log('Usage: ds <command>');
     console.log('\nAvailable commands:');
     console.log(commands.map(cmd => `\t${cmd.split('.')[0]}`).join('\n'));
+    console.log(`\nThe configuration is located at ${userHome}/.entropicrc `);
   } else {
     console.log(
       `help has not been implemented yet for ${command}. You could build it!`
