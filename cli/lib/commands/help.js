c0647c5d34f7800f932ec7141760811099d05435
Todd Kennedy <toddself@users.noreply.github.com>
2019-06-04 09:39:57 -0700

    [feat] Set up system for locale-specific help (#123)
    * [feat] Set up system for locale-specific help
    
    Provide a way to determine the locale of the user and load in a
    localized help file, defaulting to english.
    
    Add example help file from #111
    
    * Add more help texts
    
    * more help texts
    
    * eliminate console

--- cli/lib/commands/help.js
+++ cli/lib/commands/help.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
 const readdirAsync = promisify(fs.readdir);
+const accessAsync = promisify(fs.access);
 
 const userHome = require('user-home');
+const getLocale = require('os-locale');
 
 module.exports = help;
 
@@ -14,8 +16,23 @@ async function help(opts) {
   if (!command) {
     await showBasicHelp();
   } else {
-    return new Promise((resolve, reject) => {
-      fs.createReadStream(path.join(__dirname, `help-${command}.txt`))
+    return new Promise(async (resolve, reject) => {
+      const locale = (await getLocale()).toLowerCase();
+      const localeFn = path.join(__dirname, `help-${command}-${locale}.txt`);
+      const defaultFn = path.join(__dirname, `help-${command}-en_us.txt`);
+      let fn = localeFn;
+
+      try {
+        await accessAsync(localeFn);
+      } catch (err) {
+        console.log(
+          `Could not find a help file for locale ${locale}, defaulting to English`
+        );
+        console.log(`You can contribute a translation for this help file!`);
+        fn = defaultFn;
+      }
+
+      fs.createReadStream(fn)
         .on('error', async err => {
           if (err.code === 'ENOENT') {
             console.log(
