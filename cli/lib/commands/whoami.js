6ea5a6b73c2c119652076987919b233a049a2024
Sergey Petushkov <petushkov.sergey@gmail.com>
2019-06-04 22:17:17 +0200

    feat(cli): check for token before sending the request; print errors form body too; return username from method

--- cli/lib/commands/whoami.js
+++ cli/lib/commands/whoami.js
@@ -14,6 +14,15 @@ const whoamiOpts = figgy({
 async function whoami(opts) {
   opts = whoamiOpts(opts);
 
+  // If the token is missing, short-circuit and don't send
+  // the request (it will fail anyway)
+  if (!opts.token) {
+    // TODO: Maybe it makes sense to move all those messages somewhere?
+    // This also can help with localization that is discussed in #115
+    opts.log.error('You must be logged in to perform this action.');
+    return 1;
+  }
+
   const response = await fetch(`${opts.registry}/v1/auth/whoami`, {
     headers: {
       authorization: `Bearer ${opts.token}`
@@ -29,12 +38,13 @@ async function whoami(opts) {
   }
 
   if (response.status > 399) {
-    opts.log.error(body.message || body);
+    opts.log.error(body.message || body.error || body);
     return 1;
   }
 
   const { username } = body;
 
   console.log(username);
-  return 0;
+
+  return { username };
 }
