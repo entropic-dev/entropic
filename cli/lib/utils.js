3aa969fa8c99aaaa9f0f357d8b84c163ecf641c8
Nick Olinger <olingern@gmail.com>
2019-06-06 13:38:21 +0900

    Credit regex source

--- cli/lib/utils.js
+++ cli/lib/utils.js
@@ -4,6 +4,7 @@ module.exports = { createToml, tomlLocation, writeToml, isValidSemver };
 
 const fs = require('fs').promises;
 
+// Credit: https://github.com/semver/semver/issues/232#issue-48635632
 function isValidSemver(input) {
   const semverRegex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/;
   return input.match(semverRegex);
