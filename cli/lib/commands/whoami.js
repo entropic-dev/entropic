43310ef4c26ccaf1bc9b8c0b4e38975ae5205552
Nick Olinger <olingern@gmail.com>
2019-06-11 12:05:43 +0900

    [CLI] - Catch cli up Pt. 1 (#234)
    * rework whoami
    
    * add sinon
    
    * lint-fix
    
    * whoami tests and ava setup
    
    remove old mocha test
    
    * unused var
    
    * re-refactor, inject logger
    
    * require FakeLogger

--- cli/lib/commands/whoami.js
+++ cli/lib/commands/whoami.js
@@ -8,19 +8,19 @@ const { whoAmI } = require('../utils');
 const whoamiOpts = figgy({
   registry: { default: 'https://registry.entropic.dev' },
   token: true,
-  log: { default: require('npmlog') }
+  log: true,
+  api: true
 });
 
 async function whoami(opts) {
   opts = whoamiOpts(opts);
 
   try {
-    const username = await whoAmI();
-
-    console.log(username);
+    const username = await whoAmI(opts);
+    opts.log.log(username);
     return 0;
   } catch (err) {
-    console.log(err.message);
+    opts.log.error(err.message, err);
     return 1;
   }
 }
---
+++ cli/lib/logger.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+
+module.exports = class Logger {
+  static log(msg) {
+    console.log(chalk.default.yellow(msg));
+  }
+  static error(msg, stacktrace = undefined) {
+    console.log(chalk.default.red(msg));
+
+    // Don't print the stacktrace in red. That
+    // would be overwhelming
+    if (stacktrace) {
+      console.log(stacktrace);
+    }
+  }
+};
