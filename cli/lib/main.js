44548393390451ce21ede525df34fcc023b67d29
Mike Samuel <mikesamuel@gmail.com>
2019-06-12 11:30:57 -0400

    cli/lib/main now rejects command names with file metachars
    Fixes #251
    
    This adds a check to function main so that when the first argument,
    which is interpreted as a command name, has a file separator, it is
    rejected, logged, and the command `help` is used instead.
    
    To ease testing, this splits the analysis code out of `function main`
    into `function unpack` which find the `cmd` to invoke but leaves the
    invoking and error recovery to `main`.
    
    This adds tests that a dodgy command is rejected, but that others like
    `whoami` continue to function.
    
    ```sh
    $ (pushd cli; npm t; popd)
    $ npm run lint
    ```

--- cli/lib/main.js
+++ cli/lib/main.js
@@ -3,6 +3,7 @@
 'use strict';
 
 module.exports = main;
+module.exports.unpack = unpack;
 
 const minimist = require('minimist');
 
@@ -10,44 +11,55 @@ const { load } = require('./config');
 const Api = require('./api');
 const log = require('./logger');
 
-async function main(argv) {
-  if (!argv[0]) {
-    argv[0] = 'help';
+async function unpack(argv, { log = log, load = load }) {
+  let [commandName = 'help'] = argv;
+  if (/[/\\]/.test(commandName)) {
+    log.log(`Ignoring malformed command name: ${JSON.stringify(commandName)}`);
+    commandName = 'help';
   }
 
+  let cmd;
   try {
-    let cmd;
-    try {
-      cmd = require(`./commands/${argv[0]}`);
-    } catch (e) {
-      cmd = require('./commands/help');
+    cmd = require(`./commands/${commandName}`);
+  } catch (e) {
+    cmd = require('./commands/help');
+  }
+
+  const { _, ...args } = minimist(argv.slice(1));
+  const config = await load();
+  const env = {};
+  for (const key in process.env) {
+    if (key.startsWith('ent_')) {
+      env[key.slice(4)] = process.env[key];
     }
+  }
 
-    const { _, ...args } = minimist(argv.slice(1));
-    const config = await load();
-    const env = {};
-    for (const key in process.env) {
-      if (key.startsWith('ent_')) {
-        env[key.slice(4)] = process.env[key];
-      }
-    }
+  const registry = args.registry || config.registry || env.registry || 'https://registry.entropic.dev';
 
-    const registry = args.registry || config.registry || env.registry || 'https://registry.entropic.dev';
+  const registryConfig = (config.registries || {})[registry] || {};
 
-    const registryConfig = (config.registries || {})[registry] || {};
+  // env is overridden by config, which is overridden by registry-specific
+  // config, ...
+  const bundle = {
+    ...env,
+    ...config,
+    ...registryConfig,
+    ...args,
+    argv: _,
+    api: new Api(registry),
+    log
+  };
 
-    // env is overridden by config, which is overridden by registry-specific
-    // config, ...
-    await cmd({
-      ...env,
-      ...config,
-      ...registryConfig,
-      ...args,
-      argv: _,
-      api: new Api(registry),
-      log
-    });
+  return {
+    cmd,
+    bundle,
+  };
+}
 
+async function main(argv) {
+  try {
+    const { cmd, bundle } = await unpack(argv, log);
+    await cmd(bundle);
     return 0;
   } catch (err) {
     console.log(err.stack);
